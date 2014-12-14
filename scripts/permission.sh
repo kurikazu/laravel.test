0d2c9c412cd3bef8538d0ef1ab91b334355fbbaa
kurikazu <kuriu@kkuriu-mba.local>
2014-12-15 08:11:38 +0900

    sh修正

--- scripts/permission.sh
+++ scripts/permission.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod -R 777 /home/ec2-user/laraveltest/app/storage
-sudo chown -R ec2-user:ec2-user /home/ec2-user/laraveltest/
+chmod -R 777 /home/ec2-user/laraveltest/app/storage
+chown -R ec2-user:ec2-user /home/ec2-user/laraveltest
